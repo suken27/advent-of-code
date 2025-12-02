74634b1f1eec4d9de3287eae41c88f18fc4f8ac0
suken27 <suken2727@gmail.com>
2025-12-02 17:06:29 +0100

    Add solution for 2025/02 part 1

---
+++ 2025/02/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ReadFileAsStringArray(fileName string) ([]string, error) {
+	// Open the file
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan() {
+		text := scanner.Text()
+		stringArray := strings.Split(text, ",")
+		return stringArray, nil
+	} else {
+		return nil, fmt.Errorf("file: %s had no text in it", fileName)
+	}
+}
+
+func SplitRange(stringRange string) ([]int, error) {
+	stringArray := strings.Split(stringRange, "-")
+	if len(stringArray) != 2 {
+		return nil, fmt.Errorf("incorrect format for range %s", stringRange)
+	}
+	var intArray []int
+	i := 0
+	for i < len(stringArray) {
+		intValue, err := strconv.Atoi(stringArray[i])
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert string to int: %w", err)
+		}
+		intArray = append(intArray, intValue)
+		i++
+	}
+	return intArray, nil
+}
+
+func GetInvalidIds(idRange []int) []int {
+	startId := idRange[0]
+	endId := idRange[1]
+	i := startId
+	var invalidIds []int
+	for i <= endId {
+		if !IsValid(i) {
+			invalidIds = append(invalidIds, i)
+		}
+		i++
+	}
+	return invalidIds
+}
+
+func IsValid(id int) bool {
+	stringId := strconv.Itoa(id)
+	if (len(stringId) % 2) != 0 {
+		return true
+	}
+	halfLength := len(stringId) / 2
+	firsthalf := stringId[:halfLength]
+	secondHalf := stringId[halfLength:]
+	return strings.Compare(firsthalf, secondHalf) != 0
+}
+
+func main() {
+	array, error := ReadFileAsStringArray("input.txt")
+	if error != nil {
+		println(error)
+		return
+	}
+	i := 0
+	j := 0
+	result := 0
+	for i < len(array) {
+		stringRange := array[i]
+		intRange, err := SplitRange(stringRange)
+		if err != nil {
+			println(err)
+		}
+		invalidIds := GetInvalidIds(intRange)
+		j = 0
+		for j < len(invalidIds) {
+			result += invalidIds[j]
+			j++
+		}
+		i++
+	}
+	println(result)
+}
